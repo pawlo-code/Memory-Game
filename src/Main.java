964e4086fa170e5078f1663afa9565d8bc6fc200
pawlo <paaweel.22@gmail.com>
2022-07-10 09:47:08 +0200

    next upload files

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ import java.util.*;
 import java.io.*;
 public class Main {
     public static void main(String[] args) {
+
+        final String[] answers = {"A1", "A2", "A3", "A4", "B1", "B2", "B3", "B4"};
+
         ReadFile file = new ReadFile();
 
         MemoryGame game = new MemoryGame(file.getRandomWords(4));
@@ -10,16 +13,27 @@ public class Main {
 
         boolean shouldContinue = true;
 
-//        System.out.println("Press enter to START memory game!");
-//        String enter = scanner.nextLine();
-//
-//        System.out.println(file.getRandomWords(4));
-//        if (enter.equals("")) {
-//            do {
-//
-//            } while (shouldContinue);
-//        } else {
-//            System.out.println("You should just click enter...");
-//        }
+        System.out.println("Press enter to START memory game!");
+        String enter = scanner.nextLine();
+
+        if (enter.equals("")) {
+
+            do {
+                game.displayCurrentlyGame();
+                String answer = scanner.nextLine();
+
+                if (answer.equals("quit")){
+                    break;
+                } else if (Arrays.toString(answers).contains(answer) == false){
+                    System.out.println("You selected the wrong field! Try (A1-A4, B1-B4)");
+                }
+
+                game.checkGameStatus(answer);
+
+            } while (shouldContinue);
+
+        } else {
+            System.out.println("Hey.. I said Enter!");
+        }
     }
 }
--- src/MemoryGame.java
+++ src/MemoryGame.java
@@ -11,6 +11,15 @@ public class MemoryGame {
                                      {"B", "X", "X", "X", "X"},
     };
 
+    private int lastCoordinates[] = {0,0};
+
+    private boolean ifFirstAnswer = true;
+
+    private int answerCounter = 0;
+
+    private String firstAnswer = "";
+    private String secondAnswer = "";
+
     private ArrayList<String> arrayFromFile;
 
     public MemoryGame(ArrayList<String> arrayFromFile) {
@@ -35,9 +44,82 @@ public class MemoryGame {
         }
     }
 
-    public void displayCurrentlyGame(String answer){
+    public void displayCurrentlyGame() {
+        System.out.println("\n-----------------------------");
+        System.out.println("\t Level: soon");
+        System.out.println("\t Guess chances: soon");
+        //creating grid of game
+        for (int i = 0; i <= 2; i++) {
+            System.out.println("\n");
+            for (int j = 0; j <= 4; j++) {
+                //adding tabulator before some chars
+                if (displayedArray[i][j] == " " || displayedArray[i][j] == "A" || displayedArray[i][j] == "B" ){
+                    System.out.printf("\t\t" + displayedArray[i][j] + " ");
+                } else {
+                    System.out.printf(displayedArray[i][j] + " ");
+                }
+            }
+        }
+        System.out.println("\n\n-----------------------------");
 
     }
 
+    public void checkGameStatus(String answer){
+        answerCounter++;
+        switch (answer) {
+            case "A1" -> {
+                displayedArray[1][1] = hidedArray[1][1];
+                checkAnswer(1, 1);
+            }
+            case "A2" -> {
+                displayedArray[1][2] = hidedArray[1][2];
+                checkAnswer(1, 2);
+            }
+            case "A3" -> {
+                displayedArray[1][3] = hidedArray[1][3];
+                checkAnswer(1, 3);
+            }
+            case "A4" -> {
+                displayedArray[1][4] = hidedArray[1][4];
+                checkAnswer(1, 4);
+            }
+            case "B1" -> {
+                displayedArray[2][1] = hidedArray[2][1];
+                checkAnswer(2,  1);
+            }
+            case "B2" -> {
+                displayedArray[2][2] = hidedArray[2][2];
+                checkAnswer(2, 2);
+            }
+            case "B3" -> {
+                displayedArray[2][3] = hidedArray[2][3];
+                checkAnswer(2, 3);
+            }
+            case "B4" -> {
+                displayedArray[2][4] = hidedArray[2][4];
+                checkAnswer(2, 4);
+            }
+        }
+    }
 
+    private void checkAnswer(int row, int column){
+        if(ifFirstAnswer){
+            firstAnswer = displayedArray[row][column];
+            ifFirstAnswer = false;
+            lastCoordinates[0] = row;
+            lastCoordinates[1] = column;
+        }else{
+            secondAnswer = displayedArray[row][column];
+            ifFirstAnswer = true;
+        }
+        //Answer check every two rounds
+        if(answerCounter % 2 == 0){
+            if(firstAnswer == secondAnswer){
+                System.out.println("Nice you did it!");
+            } else {
+                displayedArray[row][column] = "X";
+                displayedArray[lastCoordinates[0]][lastCoordinates[1]] = "X";
+            }
+        }
+    }
 }
